4f944186bece4cfe9cab4dc5f1d57a967b18b6ab
lzcunt <lzcunt@rawr.fun>
2026-09-21 17:45:54 +0300

    options/linux: implement memalign

--- options/linux/generic/malloc.cpp
+++ options/linux/generic/malloc.cpp
@@ -1,7 +1,14 @@
+#include <bit>
 #include <bits/ensure.h>
 #include <malloc.h>
+#include <mlibc/allocator.hpp>
 
-void *memalign(size_t, size_t) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
+void *memalign(size_t align, size_t size) {
+	if (align < sizeof(void *))
+		align = sizeof(void *);
+	align = std::bit_ceil(align);
+	auto p = getAllocator().allocate(size, align);
+	if (!p)
+		return nullptr;
+	return p;
 }
