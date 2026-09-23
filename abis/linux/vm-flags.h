05de7c88b8f84f0c13cbf0d9703f05d7b5b431ae
lzcunt <lzcunt@rawr.fun>
2026-09-23 17:39:10 +0300

    abis/linux: provide `MAP_HUGE_*` hugetlb flags

--- abis/linux/vm-flags.h
+++ abis/linux/vm-flags.h
@@ -30,6 +30,22 @@
 #define MAP_SYNC      0x80000
 #define MAP_FIXED_NOREPLACE 0x100000
 
+#define MAP_HUGE_SHIFT 26
+#define MAP_HUGE_MASK 0x3f
+#define MAP_HUGE_16KB (14 << 26)
+#define MAP_HUGE_64KB (16 << 26)
+#define MAP_HUGE_512KB (19 << 26)
+#define MAP_HUGE_1MB (20 << 26)
+#define MAP_HUGE_2MB (21 << 26)
+#define MAP_HUGE_8MB (23 << 26)
+#define MAP_HUGE_16MB (24 << 26)
+#define MAP_HUGE_32MB (25 << 26)
+#define MAP_HUGE_256MB (28 << 26)
+#define MAP_HUGE_512MB (29 << 26)
+#define MAP_HUGE_1GB (30 << 26)
+#define MAP_HUGE_2GB (31 << 26)
+#define MAP_HUGE_16GB (34U << 26)
+
 #endif /* __MLIBC_LINUX_OPTION */
 
 #define MS_ASYNC 0x01
